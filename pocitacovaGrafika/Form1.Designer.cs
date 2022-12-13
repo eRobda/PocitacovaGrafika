4e37760729c2c88d4540e692891256ee9d47db6b
Robin Ustohal <ustohal.robin@student.sposdk.cz>
2022-12-13 10:18:08 +0100

    c

--- pocitacovaGrafika/Form1.Designer.cs
+++ pocitacovaGrafika/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.souborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zobrazitFotoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zobrazitFotoOVelikostiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.souborToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(986, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // souborToolStripMenuItem
+            // 
+            this.souborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zobrazitFotoToolStripMenuItem,
+            this.zobrazitFotoOVelikostiToolStripMenuItem});
+            this.souborToolStripMenuItem.Name = "souborToolStripMenuItem";
+            this.souborToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.souborToolStripMenuItem.Text = "Soubor";
+            // 
+            // zobrazitFotoToolStripMenuItem
+            // 
+            this.zobrazitFotoToolStripMenuItem.Name = "zobrazitFotoToolStripMenuItem";
+            this.zobrazitFotoToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.zobrazitFotoToolStripMenuItem.Text = "Zobrazit foto";
+            this.zobrazitFotoToolStripMenuItem.Click += new System.EventHandler(this.zobrazitFotoToolStripMenuItem_Click);
+            // 
+            // zobrazitFotoOVelikostiToolStripMenuItem
+            // 
+            this.zobrazitFotoOVelikostiToolStripMenuItem.Name = "zobrazitFotoOVelikostiToolStripMenuItem";
+            this.zobrazitFotoOVelikostiToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.zobrazitFotoOVelikostiToolStripMenuItem.Text = "Zobrazit foto o velikosti...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(986, 561);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem souborToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zobrazitFotoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zobrazitFotoOVelikostiToolStripMenuItem;
     }
 }
 
--- pocitacovaGrafika/Form1.cs
+++ pocitacovaGrafika/Form1.cs
@@ -16,5 +16,10 @@ namespace pocitacovaGrafika
         {
             InitializeComponent();
         }
+
+        private void zobrazitFotoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
